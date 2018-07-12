f3fc5b9a8b5752b1cd0b5baa5da5155e7e104a7d
Topsyaka <antonchm94@gmail.com>
2018-07-12 21:29:27 +0300

    Firs commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const API_KEY = 'fd58e48d';
+const URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
+const form = document.forms.search_form;
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const value = form.title.value.trim();
+    if (!value) {
+        form.title.classList.add('error');
+        setVisibility(form.querySelector('.error-message'), true);
+    } else {
+        fetch(`${URL}&t=${value}`)
+            .then(response => response.json())
+            .then(data => console.log(data))
+        form.title.classList.remove('error');
+        setVisibility(form.querySelector('.error-message'), false);
+    } 
+});
+
+function setVisibility(element, isError) {
+    isError ?
+        element.classList.add('visible') :
+        element.classList.remove('visible');
+}
